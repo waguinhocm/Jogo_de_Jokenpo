83b5bb2d205d44e7ea6c366898293ebc178545fb
waguinhocm <waguinhocm@hotmail.com>
2025-01-21 21:22:09 -0300

    Incluindo random.choice da biblioteca random

---
+++ main.py
@@ -0,0 +1,41 @@
+###Jogo de Jokenpo###
+import random
+
+print("Olá! Vamos jogar?")
+
+
+print("""
+pedra
+      
+papel
+      
+tesoura
+      """)
+try:
+    minhaEscolha = random.choice(["pedra", "papel", "tesoura"])
+    print("Jogador 1 já escolheu")
+    suaEscolha = input("Jogador 2 - Escolha pedra, papel ou tesoura: ")
+    if minhaEscolha == "pedra":
+        if suaEscolha == "papel":
+            print("Jogador 2 ganhou! Jogador 1 escolheu ", minhaEscolha)
+        elif suaEscolha == "tesoura":
+            print("Jogador 2 perdeu! Jogador 1 escolheu ", minhaEscolha)
+        else:
+            print("Empate! Ambos escolheram ", minhaEscolha)
+    elif minhaEscolha == "papel":
+        if suaEscolha == "pedra":
+            print("Jogador 2 perdeu! Jogador 1 escolheu ", minhaEscolha)
+        elif suaEscolha == "tesoura":
+            print("Jogador 2 ganhou! Jogador 1 escolheu ", minhaEscolha)
+        else:
+            print("Empate! Ambos escolheram ", minhaEscolha)
+    elif minhaEscolha == "tesoura":
+        if suaEscolha == "pedra":
+            print("Jogador 2 ganhou! Jogador 1 escolheu ", minhaEscolha)
+        elif suaEscolha == "papel":
+            print("Jogador 2 perdeu! Jogador 1 escolheu ", minhaEscolha)
+        else:
+            print("Empate! Ambos escolheram ", minhaEscolha)
+        
+except:
+    print("Algo deu errado!")
